85c56aebd7aa0e43b108ead35881d8d5a85349c2
Moral <jeramoral@gmail.com>
2018-05-31 06:59:31 +0000

    added css to my markdown preview

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import ReactMarkdown from 'react-markdown';
 
  class App extends React.Component {
@@ -16,15 +15,15 @@ import ReactMarkdown from 'react-markdown';
     }
 
     render() {
-       
         return (
             <div className="markdownPreviewer">
                 <h1> Markdown Previewer</h1>
                 <textarea rows="18" cols="48" onChange={this.handleChange.bind(this)} />
                 <ReactMarkdown source={this.state.source} />   
             </div>
-
         );
     }
 }
+
+
 export default App;
