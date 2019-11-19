e5d6565f3064df67d2d2af4876b70a436d599374
moralss <jeramoral@gmail.com>
2019-11-19 13:45:54 +0200

    Removed redux thunk example

--- redux-example/src/actions/actions.js
+++ redux-example/src/actions/actions.js
@@ -14,35 +14,12 @@ export const subtractCounter = () => {
   }
 }
 
-// export const saveMessage = (payload) => {
-//   return {
-//     type: actions.SAVE_MESSAGES,
-//     payload
-
-//   }
-// }
-
 
 export const saveMessage = (payload) => {
   return async dispatch => {
-    try {
-      const {
-        data
-      } = await axios.get(
-        "http://ac88a44a6935711e982b602f197ebe6f-1529281652.eu-west-2.elb.amazonaws.com/person/"
-      )
-      dispatch({
-        type: actions.SAVE_MESSAGES,
-        payload: data
-      })
-      console.log("data", data);
 
-    } catch (e) {
-      console.log(e);
-      dispatch({
-        type: actions.Error,
-        e
-      })
-    }
+    dispatch({
+      type: "moral"
+    })
   }
 }
